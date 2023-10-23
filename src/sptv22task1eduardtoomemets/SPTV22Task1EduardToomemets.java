7985f2c663a0f079d58ede816f304fe27b89eb07
Eduard <your@example.com>
2023-10-23 11:01:56 +0300

    first commit

---
+++ src/sptv22task1eduardtoomemets/SPTV22Task1EduardToomemets.java
@@ -0,0 +1,9 @@
+package sptv22task1eduardtoomemets;
+
+
+public class SPTV22Task1EduardToomemets {
+
+    public static void main(String[] args) {
+        System.out.println("Java Hello!");
+    }
+}
